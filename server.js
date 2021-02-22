9d65b04c47de4e134d3815c0e2cdd48ed80957c2
David Mariano Gimenez Mota <davidmariangmota@gmail.com>
2021-02-22 10:00:11 -0300

    teste

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const app = express();
 const cors = require('cors')
 const mysql = require('mysql2');
 
+const connection = mysql.createConnection({
+  host:'localhost',
+  user: 'root',
+  database: 'bregister',
+  port: '3308',
+  password: null
+});
+
 app.use(cors())
 
+const PORTA = process.env.SERVER_PORT || 3000;
 
-const connection = mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    database: 'bregister',
-    port: '3308',
-    password: null
-});
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html')
+})
 
 
 app.get('/api/vagas', function(req, res){
@@ -25,6 +30,6 @@ app.get('/api/vagas', function(req, res){
       );
 })
 
-app.listen(3030, function(){
-    console.log("Servidor rodando na porta 3030")
+app.listen(PORTA, function(){
+    console.log(`Servidor rodando na porta ${PORTA}`)
 })
